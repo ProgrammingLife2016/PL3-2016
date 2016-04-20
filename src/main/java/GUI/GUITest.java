70c15af942fe28213f2f535fef714ebbe4cdd0db
Hugo Kooijman <hugokooijman@Hugos-MacBook-Pro.local>
2016-04-20 15:52:23 +0200

    Fiddled with menu bar

--- src/main/java/GUI/GUITest.java
+++ src/main/java/GUI/GUITest.java
@@ -17,52 +17,77 @@ import javafx.scene.text.Font;
 import javafx.stage.Stage;
 import javafx.geometry.*;
 
-//Window is called the "Stage"
-//Content inside window is called "Scene"
-
 @SuppressWarnings("restriction")
 public class GUITest extends Application {
 	
-	Button loadbutton;
-	Button exitbutton;
 	private LoadControl loadcontrol = new LoadControl();
 	private ExitControl exitcontrol = new ExitControl();
 	
+	/**
+	 * Launch application.
+	 * 
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		launch(args);
 	}
-
+	
+	/**
+	 * GUI setup.
+	 */
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-		primaryStage.setTitle("DNAnalyzer");
+		primaryStage.setTitle("DNAnalyzer");  //Stage = window
 		
-		StackPane layout = new StackPane();
-		VBox buttonpanel = createButtonPanel();
-		layout.getChildren().add(buttonpanel);
+		BorderPane layout = new BorderPane();
+		MenuBar mainMenuBar = createMainMenuBar();
 		
-		Scene scene = new Scene(layout, 700, 400); //layout, sizeh, sizev
+		layout.setTop(mainMenuBar);
+		layout.setStyle("-fx-background-color: #228899;");
 		
-		primaryStage.setScene(scene); 
+		Scene scene = new Scene(layout, 1000, 400); //layout, sizeh, sizev
+		
+		primaryStage.setScene(scene); //Scene = inside window
 		primaryStage.show(); //shows stage to user
 	}	
 	
-	public VBox createButtonPanel() {
-	    VBox vbox = new VBox();
-	    vbox.setPadding(new Insets(15, 12, 15, 12));
-	    vbox.setSpacing(10);
-	    vbox.setStyle("-fx-background-color: #336699;");
-
-		loadbutton = new Button();
-		loadbutton.setText("Load DNA"); //Name of button
-		loadbutton.setOnAction(loadcontrol); //If clicked, run "handle" located in "this" class
-		loadbutton.setPrefSize(100, 50);
+	public MenuBar createMainMenuBar() {
+		Menu mainmenu = new Menu("File");
 		
-		exitbutton = new Button();
-		exitbutton.setText("Exit");
-		exitbutton.setOnAction(exitcontrol);
-		exitbutton.setPrefSize(100, 50);
-
-	    vbox.getChildren().addAll(loadbutton, exitbutton);
-	    return vbox;
+		MenuItem menuload = new MenuItem("Load...");
+		MenuItem menuexit = new MenuItem("Exit");
+		menuload.setOnAction(loadcontrol);
+		menuexit.setOnAction(exitcontrol);
+		
+		SeparatorMenuItem separator = new SeparatorMenuItem();
+		
+		mainmenu.getItems().add(menuload);
+		mainmenu.getItems().add(separator);
+		mainmenu.getItems().add(menuexit);
+		
+		MenuBar mainmenubar = new MenuBar();
+		mainmenubar.getMenus().add(mainmenu);
+		
+		return mainmenubar;
 	}
+	
+//	public VBox createButtonPanel() {
+//	    VBox vbox = new VBox();
+//	    vbox.setPadding(new Insets(15, 15, 15, 15));
+//	    vbox.setSpacing(10);
+//	    vbox.setStyle("-fx-background-color: #336699;");
+//
+//		loadbutton = new Button();
+//		loadbutton.setText("Load DNA"); //Name of button
+//		loadbutton.setOnAction(loadcontrol); //If clicked, run "handle" located in "this" class
+//		loadbutton.setPrefSize(100, 50);
+//		
+//		exitbutton = new Button();
+//		exitbutton.setText("Exit");
+//		exitbutton.setOnAction(exitcontrol);
+//		exitbutton.setPrefSize(100, 50);
+//
+//	    vbox.getChildren().addAll(loadbutton, exitbutton);
+//	    return vbox;
+//	}
 }
