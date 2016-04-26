031203144d3b1282b0b225e63059100ddadee6ef
Hugo Kooijman <hugokooijman@wlan-145-94-207-152.wlan.tudelft.nl>
2016-04-26 12:59:09 +0200

    Merged GUItryouts with SymanticZoom

--- src/main/java/gui/Gui.java
+++ src/main/java/gui/Gui.java
@@ -1,5 +1,8 @@
 package gui;
 
+import java.io.File;
+
+import main.Main;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
@@ -18,19 +21,10 @@ import javafx.stage.Stage;
 import javafx.geometry.*;
 
 @SuppressWarnings("restriction")
-public class GUITest extends Application {
+public class Gui extends Application {
 	
 	private LoadControl loadcontrol = new LoadControl();
 	private ExitControl exitcontrol = new ExitControl();
-	
-	/**
-	 * Launch application.
-	 * 
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		launch(args);
-	}
 	
 	/**
 	 * GUI setup.
@@ -38,14 +32,20 @@ public class GUITest extends Application {
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 		primaryStage.setTitle("DNAnalyzer");  //Stage = window
+		String stylepath = "Styles/menubar.css";
 		
 		BorderPane layout = new BorderPane();
 		MenuBar mainMenuBar = createMainMenuBar();
+		mainMenuBar.getStylesheets().add(stylepath);
+		mainMenuBar.getStyleClass().add("menubar");
+		//mainMenuBar.setStyle("-fx-text-fill: #6DDB07; -fx-background-color: black;");
 		
 		layout.setTop(mainMenuBar);
-		layout.setStyle("-fx-background-color: #228899;");
+		//layout.setStyle("-fx-background-color: #6DDB07;");
+		layout.getStyleClass().add("scene");
 		
 		Scene scene = new Scene(layout, 1000, 400); //layout, sizeh, sizev
+		scene.getStylesheets().add(stylepath);
 		
 		primaryStage.setScene(scene); //Scene = inside window
 		primaryStage.show(); //shows stage to user
@@ -53,9 +53,14 @@ public class GUITest extends Application {
 	
 	public MenuBar createMainMenuBar() {
 		Menu mainmenu = new Menu("File");
+		mainmenu.getStyleClass().add("menutext");
+		//mainmenu.setStyle("-fx-background-colour: #6DDB07; -fx-text-fill: #6DDB07;");
 		
 		MenuItem menuload = new MenuItem("Load...");
 		MenuItem menuexit = new MenuItem("Exit");
+		menuload.getStyleClass().add("button");
+		menuexit.getStyleClass().add("button");
+		
 		menuload.setOnAction(loadcontrol);
 		menuexit.setOnAction(exitcontrol);
 		
@@ -70,24 +75,8 @@ public class GUITest extends Application {
 		
 		return mainmenubar;
 	}
-	
-//	public VBox createButtonPanel() {
-//	    VBox vbox = new VBox();
-//	    vbox.setPadding(new Insets(15, 15, 15, 15));
-//	    vbox.setSpacing(10);
-//	    vbox.setStyle("-fx-background-color: #336699;");
-//
-//		loadbutton = new Button();
-//		loadbutton.setText("Load DNA"); //Name of button
-//		loadbutton.setOnAction(loadcontrol); //If clicked, run "handle" located in "this" class
-//		loadbutton.setPrefSize(100, 50);
-//		
-//		exitbutton = new Button();
-//		exitbutton.setText("Exit");
-//		exitbutton.setOnAction(exitcontrol);
-//		exitbutton.setPrefSize(100, 50);
-//
-//	    vbox.getChildren().addAll(loadbutton, exitbutton);
-//	    return vbox;
-//	}
+
+	public void handle(ActionEvent event) {
+		// TODO Auto-generated method stub
+	}
 }
---
+++ src/main/java/gui/TutorialSceneswitch.java
@@ -0,0 +1,70 @@
+package gui;
+
+import java.io.File;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Pos;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.effect.DropShadow;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.stage.Stage;
+import javafx.geometry.*;
+
+public class TutorialSceneswitch extends Gui implements EventHandler<ActionEvent> {
+	
+	Stage window;
+	Scene scene1, scene2;
+	Button button, button2;
+	
+	@SuppressWarnings("restriction")
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		window = primaryStage;
+		
+		//Buttons
+		button = new Button("Switch to Scene 2");
+		button.setOnAction(this);
+		button2 = new Button("Back to Scene 1");
+		button2.setOnAction(this);
+		
+		//Labels
+		Label label1 = new Label("Scene 1");
+		Label label2 = new Label("Scene 2");
+		
+		
+		VBox layout1 = new VBox(20);
+		layout1.getChildren().addAll(label1, button);
+		scene1 = new Scene(layout1, 200, 200);
+		
+		StackPane layout2 = new StackPane();
+		layout2.getChildren().add(button2);
+		scene2 = new Scene(layout2, 200, 200);
+		
+		window.setScene(scene1);
+		window.setTitle("Switch scene test");
+		window.show();
+	}
+	
+	@SuppressWarnings("restriction")
+	@Override
+	public void handle(ActionEvent event) {
+		if (event.getSource() == button ) {
+			window.setScene(scene2);
+		}
+		if (event.getSource() == button2 ) {
+			window.setScene(scene1);
+		}
+	}
+	
+	
+	
+}
