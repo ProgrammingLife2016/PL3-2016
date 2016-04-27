bdcd309da135bac1a67664181f565fc728d0f6c7
Evan <pickleo.joe@gmail.com>
2016-04-27 15:31:30 +0200

    Cleaning up and checkstyle fixes

--- src/main/java/GUI/SymanticZoom/SymanticZoomTest.java
+++ src/main/java/GUI/SymanticZoom/SymanticZoomTest.java
@@ -2,18 +2,12 @@ package GUI.SymanticZoom;
 import java.util.ArrayList;
 
 import javafx.application.Application;
-import javafx.beans.property.DoubleProperty;
-import javafx.beans.property.SimpleDoubleProperty;
 import javafx.event.EventHandler;
 import javafx.scene.Group;
-import javafx.scene.Node;
 import javafx.scene.Scene;
-import javafx.scene.canvas.Canvas;
-import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Label;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.input.ScrollEvent;
-import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 import javafx.scene.shape.Rectangle;
