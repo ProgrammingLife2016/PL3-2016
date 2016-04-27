f24c3c172d788be649ceb6560d979ab59365e2e1
Rob Kapel <R.W.Kapel@student.tudelft.nl>
2016-04-27 16:40:25 +0200

    Added simple coordinate system for genomes

---
+++ src/main/java/coordinates/Coordinate.java
@@ -0,0 +1,19 @@
+package coordinates;
+
+public class Coordinate {
+	private int x;
+	private int y;
+	
+	public Coordinate(int xCoord, int yCoord) {
+		x = xCoord;
+		y = yCoord;
+	}
+	
+	public int getX() {
+		return x;
+	}
+	
+	public int getY() {
+		return y;
+	}
+}
