30f9012b43294006d99edf75273999c202f503f9
B.Ho <bjornho@outlook.com>
2016-05-02 17:01:58 +0200

    Not working
    A line gets drawn but all existing features are broken :S.

---
+++ src/main/java/gui/ScreenManager.java
@@ -0,0 +1,134 @@
+package gui;
+
+import java.util.HashMap;
+import javafx.animation.KeyFrame;
+import javafx.animation.KeyValue;
+import javafx.animation.Timeline;
+import javafx.beans.property.DoubleProperty;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.layout.StackPane;
+import javafx.util.Duration;
+
+public class ScreenManager extends StackPane {
+    private HashMap<String, Node> screens = new HashMap<>();
+
+    public ScreenManager() {
+        super();
+    }
+
+    /**
+     * Add a screen to a hashmap
+     * 
+     * @param name
+     *            name of the screen
+     * @param screen
+     *            loaded screen
+     */
+    public void addScreen(String name, Node screen) {
+        screens.put(name, screen);
+    }
+
+    /**
+     * 
+     * @param name
+     *            name of the screen
+     * @return returns the node with the given name from the hashmap
+     */
+    public Node getScreen(String name) {
+        return screens.get(name);
+    }
+
+    /**
+     * It loads a FXML file and adds the screen to the screen hashmap
+     * 
+     * @param name
+     *            name of the screen
+     * @param resource
+     *            name of the fxml file
+     * @return returns true if screen loading succeeds else false
+     */
+    public boolean loadScreen(String name, String resource) {
+        try {
+            FXMLLoader myLoader = new FXMLLoader(getClass().getResource(resource));
+            Parent loadScreen = (Parent) myLoader.load();
+            SetScreen myScreenControler = ((SetScreen) myLoader.getController());
+            myScreenControler.setScreenDriver(this);
+            addScreen(name, loadScreen);
+            return true;
+        } catch (Exception e) {
+			System.err.println("Failed to load screen " + getClass().getResource(resource));
+			e.printStackTrace();
+            return false;
+        }
+    }
+
+    /**
+     * checks if the screen has already been loaded and switches screen with
+     * screen transistion
+     * 
+     * @param name
+     *            screen name
+     * @return returns true if setting the screen succeeds else false
+     */
+    public boolean setScreen(final String name) {
+        if (screens.get(name) != null) {
+            final DoubleProperty opacity = opacityProperty();
+            // if there is more than one screen
+            if (!getChildren().isEmpty()) { 
+            	//the duration is the fade out time
+            	Timeline fade = new Timeline(new KeyFrame(Duration.ZERO,
+            		new KeyValue(opacity, 1.0)), new KeyFrame(new Duration(
+                    250), new EventHandler<ActionEvent>() { 
+                           
+            			@Override
+            			public void handle(ActionEvent t) {
+            			//removing the displayed screen
+	                    getChildren().remove(0); 
+	                    //adds the new screen
+	                    getChildren().add(0, screens.get(name));
+	                    //fade in time
+	                    Timeline fadeIn = new Timeline(new KeyFrame(Duration.ZERO, new KeyValue(opacity,
+	                    		0.7)), new KeyFrame(new Duration(250), new KeyValue(opacity, 1.0)));
+	                    fadeIn.play();
+	                    }
+                    }, new KeyValue(opacity, 0.7)));
+                fade.play();
+            }
+            else {
+                setOpacity(0.7);
+                getChildren().add(screens.get(name));
+                Timeline fadeIn = new Timeline(new KeyFrame(Duration.ZERO,
+                		new KeyValue(opacity, 0.7)), new KeyFrame(new Duration(
+                        250), new KeyValue(opacity, 1.0)));
+                fadeIn.play();
+            }
+            return true;
+        }
+        else {
+			System.err.println("Could not set screen " + name);
+            return false;
+        }
+    }
+
+    /**
+     * this removes (unloads) a screen from the hashmap of screens
+     * 
+     * @param name
+     *            screen name
+     * @return returns a boolean
+     */
+    public boolean unloadScreen(String name) {
+        if (screens.remove(name) == null) {
+			System.err.println("Screen didn't exist");
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+}
+
---
+++ src/main/java/gui/SetScreen.java
@@ -0,0 +1,6 @@
+package gui;
+
+public interface SetScreen {
+	  // This method will allow the injection of the ScreenPane
+    public void setScreenDriver(ScreenManager screenPage);
+}
