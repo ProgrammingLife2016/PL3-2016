3ddee30fa62a552160a10419db41d926c18c430f
Hugo Kooijman <H.V.Kooijman@gmail.com>
2016-05-08 00:51:36 +0200

    Added basic drawing functionality
    Segments are being drawn on the screen now. Still need to fix alignment.

--- src/main/java/gui/GraphController.java
+++ src/main/java/gui/GraphController.java
@@ -22,6 +22,9 @@ import javafx.scene.shape.MoveTo;
 import javafx.scene.shape.Path;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.shape.StrokeType;
+import javafx.scene.text.Text;
+
+
 
 import java.net.URL;
 import java.util.ArrayList;
@@ -36,7 +39,7 @@ public class GraphController implements Initializable {
 	private DatabaseManager dbm;
 	
 	private static final int ROOT_X_OFFSET = 40;
-	private static final int CHILD_X_OFFSET = 200;
+	private static final int CHILD_X_OFFSET = 100;
 	
 	private int[] from = {1, 1, 2, 3, 4, 4, 4, 5, 6, 7};
 	private int[] to = {2, 3, 4, 4, 5, 6, 7, 8, 8, 8};
@@ -49,22 +52,23 @@ public class GraphController implements Initializable {
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
 		this.dbm = Launcher.dbm;
-		drawRootSegment();
 		
-		for (int i = 0; i < 5; i++) {
-			//graphpane.getChildren().add(segment);
-		}
 		graphpane.setPrefWidth(10000);
+		graphpane.setPrefHeight(1000);
+		segments = constructSegmentMap();
+		drawGraph();
 	}
 	
 	public HashMap<Integer, GraphSegment> constructSegmentMap() { 
 		HashMap<Integer, GraphSegment> segments = new HashMap<Integer,
 				GraphSegment>((int) Math.ceil(to.length / 0.75));
-		int linkpointer = 0;
+		int linkpointer = 1;
 		
 		for (int i = 1; i <= getSegmentCount(); i++) {
 			int childcount = 0;
-			while (i == from[linkpointer]) {
+			System.out.println("LinkPointer is now " + linkpointer);
+			while (linkpointer <= from.length && i == from[linkpointer - 1]) {
+				System.out.println("SegmentId now: " + i);
 				childcount++;
 				linkpointer++;
 			}
@@ -72,24 +76,46 @@ public class GraphController implements Initializable {
 			
 			GraphSegment segment = new GraphSegment(i, childcount);
 			int childindex = 0;
-			while (i == from[linkpointer]) {
-				segment.getSegmentChildren()[childindex] = to[linkpointer];
+			while (linkpointer <= from.length && i == from[linkpointer - 1]) {
+				System.out.println("Going to: " + to[linkpointer - 1]);
+				segment.getSegmentChildren()[childindex] = to[linkpointer - 1];
 				linkpointer++;
+				childindex++;
 			}
+			System.out.println("Now putting " + i + " in hashmap");
 			segments.put(i, segment);
 		}
 		return segments;
+	}
+	
+	public void drawGraph() {
+		GraphSegment root = segments.get(1);
+		drawRootSegment();
+		drawChildSegments();
+		for (int i = 1; i <= segments.size(); i++) {
+			Text text = new Text("" + segments.get(i).getSegmentId());
+			segments.get(i).getChildren().add(text);
+			graphpane.getChildren().add(segments.get(i));
+		}	
 	}
 	
 	public void drawRootSegment() {
 		GraphSegment root = segments.get(1);
 		root.setLayoutX(ROOT_X_OFFSET);
 		root.setLayoutY((int) graphpane.getPrefHeight() / 2); //Middle of Y-axis
-		graphpane.getChildren().add(root);
 	}
 	
-	public void drawChildSegments(GraphSegment root) {
-
+	
+	public void drawChildSegments() {
+		for (int j = 1; j <= segments.size(); j++) {
+			for (int i = 0; i < segments.get(j).getSegmentChildren().length; i++) {
+				System.out.println("Drawing segmentchild " + segments.get(j).getSegmentChildren()[i] +
+						" of: " + segments.get(j).getSegmentId());
+				GraphSegment seg = segments.get(segments.get(j).getSegmentChildren()[i]);
+				seg.setLayoutX(segments.get(j).getLayoutX() + CHILD_X_OFFSET);
+				seg.setLayoutY(segments.get(j).getLayoutY() + 70 * i);
+			}
+		}
 	}
 	
 	public int getSegmentCount() {
@@ -103,7 +129,5 @@ public class GraphController implements Initializable {
 	    segment.setLayoutY(200);
 		return segment;
 	}
-	
-	
 
 }
