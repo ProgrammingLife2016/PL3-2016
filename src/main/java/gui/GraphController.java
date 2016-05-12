4edfac9d6ce306d4300c2613c1d3b0e47e5e4f00
Hugo Kooijman <H.V.Kooijman@gmail.com>
2016-05-12 14:26:30 +0200

    Refactored GraphController
    Divided up code into multiple methods as to increase readability. Also adjusted the way GraphSegments are drawn. They are now placed onto the graph after all the paths are drawn. This prevents paths being drawn over the segments.

--- src/main/java/gui/GraphController.java
+++ src/main/java/gui/GraphController.java
@@ -1,113 +1,125 @@
 package gui;
 
 import db.DatabaseManager;
+
 import javafx.fxml.FXML;
+import javafx.scene.Group;
 import javafx.fxml.Initializable;
-import javafx.scene.layout.AnchorPane;
 import javafx.scene.shape.LineTo;
 import javafx.scene.shape.MoveTo;
 import javafx.scene.shape.Path;
-import javafx.scene.text.Text;
 
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.ResourceBundle;
 
+/**
+ * @author hugokooijman
+ *
+ * Class containing all the logic involving the graph view. It reads out database tables,
+ * stores required information in memory and adds the required to the Group "graphpane",
+ * which is defined in the GraphLevel.fxml file. This will result in a visual representation
+ * of all segments that are loaded into it.
+ */
 @SuppressWarnings("restriction")
 public class GraphController implements Initializable {
-	@FXML AnchorPane graphpane;
+	
+	/**
+	 * Group in fxml file to which GraphSegments will be added.
+	 */
+	@FXML Group graphpane;
+	
+	/**
+	 * DatabaseManager for reading out required data.
+	 */
 	private DatabaseManager dbm;
 	
 	/**
 	 * Map of all GraphSegments.
 	 */
 	private HashMap<Integer, GraphSegment> segments;
+	
+	/**
+	 * 5 lists of required data for constructing GraphSegments.
+	 */
+	private ArrayList<Integer> from;
+	private ArrayList<Integer> to;
+	private ArrayList<Integer> graphxcoords;
+	private ArrayList<Integer> graphycoords;
+	private ArrayList<String> segmentdna;
 
+	/**
+	 * Initialize fxml file.
+	 */
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
 		this.dbm = Launcher.dbm;
-		
-		ArrayList<Integer> from = dbm.getDbReader().getAllFromId();
-		ArrayList<Integer> to = dbm.getDbReader().getAllToId();
-		ArrayList<Integer> graphxcoords =
-				scaleRibbonToGraphCoordsX(dbm.getDbReader().getAllXCoord());
-		ArrayList<Integer> graphycoords =
-				scaleRibbonToGraphCoordsY(dbm.getDbReader().getAllYCoord());
-		
-		segments = constructSegmentMap(from, to, graphxcoords, graphycoords);
-		drawGraph(from, to, graphxcoords, graphycoords);
-		
-		graphpane.setPrefWidth(segments.get(to.get(to.size() - 1)).getLayoutX());
-		graphpane.setPrefHeight(1500);
+		loadSegmentData();
+		constructSegmentMap();
+		drawGraph();
 	}
 	
-	public HashMap<Integer, GraphSegment> constructSegmentMap(ArrayList<Integer> from,
-			ArrayList<Integer> to, ArrayList<Integer> xcoords, ArrayList<Integer> ycoords) {
-		HashMap<Integer, GraphSegment> segments = new HashMap<Integer,
-				GraphSegment>((int) Math.ceil(to.size() / 0.75));
+	/**
+	 * Load in all necessary information from the datbase.
+	 */
+	private void loadSegmentData() {
+		from = dbm.getDbReader().getAllFromId();
+		to = dbm.getDbReader().getAllToId();
+		graphxcoords = scaleRibbonToGraphCoordsX(dbm.getDbReader().getAllXCoord());
+		graphycoords = scaleRibbonToGraphCoordsY(dbm.getDbReader().getAllYCoord());
+		
+		//The loadfactor of a standard java hash map is 0.75. By setting the hash map size
+		//this way, less memory is wasted by the hashmap, for not doubling storage capacity.
+		segments = new HashMap<Integer, GraphSegment>((int) Math.ceil(to.size() / 0.75));
+		
+		segmentdna = new ArrayList<String>();
+		for (int i = 1; i <= to.get(to.size() - 1); i++) {
+			segmentdna.add(dbm.getDbReader().getContent(i));
+		}
+	}
+	
+	/**
+	 * Creates a hash map of all segments, by storing their information in GraphSegment
+	 * objects.
+	 */
+	public void constructSegmentMap() {
 		int linkpointer = 1;
 		
+		//Loop over all segment id's.
 		for (int i = 1; i <= to.get(to.size() - 1); i++) {
 			int childcount = 0;
+			
+			//Figure out amount of childs of segment.
 			while (linkpointer <= from.size() && i == from.get(linkpointer - 1)) {
 				childcount++;
 				linkpointer++;
 			}
 			linkpointer -= childcount;
 			
-			String content = dbm.getDbReader().getContent(i);
-			char[] dnacontent = content.toCharArray();
+			GraphSegment segment = new GraphSegment(i, childcount,
+					segmentdna.get(i - 1).toCharArray(), graphxcoords.get(i - 1),
+					graphycoords.get(i - 1));
 			
-			GraphSegment segment = new GraphSegment(i, childcount, dnacontent);
 			int childindex = 0;
 			while (linkpointer <= from.size() && i == from.get(linkpointer - 1)) {
 				segment.getSegmentChildren()[childindex] = to.get(linkpointer - 1);
 				linkpointer++;
 				childindex++;
 			}
-			Text text = new Text();
-
-			String dna = "";
-			for (int j = 0; j < segment.getDnacontent().length && j <= 4; j++) {
-				dna += "" + segment.getDnacontent()[j];
-			}
-			if (segment.getDnacontent().length > 5) {
-				dna += "...";
-			}
-			text.setText(dna);
-			segment.getChildren().add(text);
-			
-			segment.setLayoutX(xcoords.get(i - 1));
-			segment.setLayoutY(ycoords.get(i - 1));	
+			System.out.println("Putting segment: " + i);
 			segments.put(i, segment);
 		}
-		return segments;
+		System.out.println("TOTAL SEGMENTS: " + to.get(to.size()-1));
 	}
 	
-	public void drawGraph(ArrayList<Integer> from, ArrayList<Integer> to, 
-			ArrayList<Integer> xcoords, ArrayList<Integer> ycoords) {
-		
-		for (int i = 0; i < from.size(); i++) {
-			int fromId = from.get(i);
-			int toId = to.get(i);
-			GraphSegment fromsegment = segments.get(fromId);
-			GraphSegment tosegment = segments.get(toId);
-			Path path = drawGraphPath(fromsegment.getLayoutX() + fromsegment.getRadius(),
-					fromsegment.getLayoutY() + fromsegment.getRadius(),
-					tosegment.getLayoutX() + tosegment.getRadius(),
-					tosegment.getLayoutY() + tosegment.getRadius());
-	        path.setStrokeWidth(1);
-
-	        graphpane.getChildren().add(path);
-	        if (segments.get(fromId).isDrawn()) {
-	        	graphpane.getChildren().remove(segments.get(fromId));
-	        	graphpane.getChildren().add(segments.get(fromId));
-	        } else {
-	        	graphpane.getChildren().add(segments.get(fromId));
-	        	segments.get(fromId).setDrawn(true);
-	        }
-		}
+	/**
+	 * Uses the created hash map of segments to draw a visual representation of the segments,
+	 * such as where they are located, how they are related and what their DNA strand is.
+	 */
+	public void drawGraph() {
+		drawGraphEdges();
+		drawGraphSegments();
 	}
 	
 	/**
@@ -119,7 +131,7 @@ public class GraphController implements Initializable {
 	 * 			Segment to which path is drawn.	
 	 * @return A Path through which the lines goes.
 	 */
-	private Path drawGraphPath(double fromX, double fromY, double toX, double toY) {
+	private Path drawPath(double fromX, double fromY, double toX, double toY) {
 		MoveTo moveto = new MoveTo(fromX, fromY);
 		LineTo lineto = new LineTo(toX , toY);
 		Path path = new Path();
@@ -127,6 +139,41 @@ public class GraphController implements Initializable {
 		return path;
 	}
 	
+	/**
+	 * Draws edges between all the segment coordinates.
+	 */
+	private void drawGraphEdges() {
+		for (int i = 0; i < from.size(); i++) {
+			int fromId = from.get(i);
+			int toId = to.get(i);
+			GraphSegment fromsegment = segments.get(fromId);
+			GraphSegment tosegment = segments.get(toId);
+			Path path = this.drawPath(fromsegment.getLayoutX() + fromsegment.getRadius(),
+					fromsegment.getLayoutY() + fromsegment.getRadius(),
+					tosegment.getLayoutX() + tosegment.getRadius(),
+					tosegment.getLayoutY() + tosegment.getRadius());
+	        path.setStrokeWidth(1);
+	        graphpane.getChildren().add(path);
+		}
+	}
+	
+	/**
+	 * Draws all GraphSegments on the segment coordinates.
+	 */
+	private void drawGraphSegments() {
+		for (int i = 1; i <= segments.size(); i++) {
+			graphpane.getChildren().add(segments.get(i));
+		}
+	}
+	
+	/**
+	 * Scales and re-uses the x-coordinates calculated for the ribbon visualization.
+	 * 
+	 * @param xcoords
+	 * 			x-coordinates of ribbons.
+	 * @return xcoords
+	 * 			x-coordinates of graph segments.
+	 */
 	public ArrayList<Integer> scaleRibbonToGraphCoordsX(ArrayList<Integer> xcoords) {
 		for (int i = 0; i < xcoords.size(); i++) {
 			int newc = xcoords.remove(i) * 100;
@@ -135,6 +182,14 @@ public class GraphController implements Initializable {
 		return xcoords;
 	}
 	
+	/**
+	 * Scales and re-uses the x-coordinates calculated for the ribbon visualization.
+	 * 
+	 * @param ycoords
+	 * 			y-coordinates of ribbons.
+	 * @return ycoords
+	 * 			y-coordinates of graph segments.
+	 */
 	public ArrayList<Integer> scaleRibbonToGraphCoordsY(ArrayList<Integer> ycoords) {
 		for (int i = 0; i < ycoords.size(); i++) {
 			int newc = ycoords.remove(i) * 50;
