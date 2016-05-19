2ddb4852f62d76b9f8ddbfbfa1e6e77964967753
B.Ho <bjornho@outlook.com>
2016-05-19 15:41:21 +0200

    Added write recent test

--- src/test/java/toolbar/RecentHandlerTest.java
+++ src/test/java/toolbar/RecentHandlerTest.java
@@ -6,6 +6,7 @@ import java.io.File;
 import java.io.FilenameFilter;
 import java.io.IOException;
 
+import org.apache.commons.io.FilenameUtils;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
@@ -17,10 +18,18 @@ public class RecentHandlerTest {
 	static String directory= System.getProperty("user.dir") 
 			+ File.separator + "recent" + File.separator;
 	
+	static String dbDir= System.getProperty("user.dir") 
+			+ File.separator + "db" + File.separator;
+	
+	
 	@BeforeClass
 	public static void cleanUp() {
 		for(File file: new File(directory).listFiles()) {
 			if(!file.getName().equals(".gitignore"))
+			file.delete();
+		}
+		
+		for(File file: new File(dbDir).listFiles()) {
 			file.delete();
 		}
 	}
@@ -30,7 +39,36 @@ public class RecentHandlerTest {
 		RecentHandler recent = new RecentHandler();
 		File file = new File(directory + "recent.txt");
 		recent.buildRecent("test", "test");
-		assertEquals(true, file.exists());
+		assertTrue(file.exists());
+		cleanUp();
+	}
+	
+	@Test
+	public void testFileNotExist() {
+		RecentHandler recent = new RecentHandler();
+		assertFalse(recent.fileExists("TB500" + " " + directory + "TB500.mv.db"));
+	}
+	
+	@Test
+	public void testFileExist() throws IOException {
+		RecentHandler recent = new RecentHandler();
+		File file = new File(directory + "TB500.mv.db");
+		file.createNewFile();
+		assertTrue(recent.fileExists("TB500" + " " + directory + "TB500"));
+		cleanUp();
+	}
+	
+	@Test
+	public void testWriteRecent() throws IOException {
+		File file = new File(directory + "recent.txt");
+		file.createNewFile();
+		File file2 = new File(dbDir + "TB100.mv.db");
+		String temp = FilenameUtils.removeExtension(file2.getName());
+		String name = FilenameUtils.removeExtension(temp);
+		file2.createNewFile();
+		RecentHandler recent = new RecentHandler();
+		recent.writeRecent(file, dbDir + name, name);
+		assertEquals(dbDir + name, recent.getRecent().get(name));
 		cleanUp();
 	}
 }
