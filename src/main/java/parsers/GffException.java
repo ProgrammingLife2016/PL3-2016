1d897e69ff85c23993445da041a433d3b6d5c77d
Evan <pickleo.joe@gmail.com>
2016-06-01 13:42:50 +0200

    Created parser exception

---
+++ src/main/java/parsers/GffException.java
@@ -0,0 +1,10 @@
+package parsers;
+
+/**
+ * Exception for the GFA parser to use.
+ */
+public class GffException extends Exception {
+	
+	private static final long serialVersionUID = -2735191969133065579L;
+
+}
--- src/main/java/parsers/GffParser.java
+++ src/main/java/parsers/GffParser.java
@@ -1,5 +1,80 @@
 package parsers;
 
-public class GffParser {
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
+import db.DatabaseManager;
+import db.tables.AnnotationTable;
+import db.tables.BubbleTable;
+import db.tables.GenomeSegmentLinkTable;
+import db.tables.GenomeTable;
+import db.tables.LinkTable;
+import db.tables.SegmentTable;
+import db.tables.Table;
+import db.tuples.GenomeSegmentLinkTuple;
+import db.tuples.GenomeTuple;
+import db.tuples.LinkTuple;
+import db.tuples.SegmentTuple;
+
+public class GffParser {
+	
+	/**
+	 * Required DatabaseManager for loading parsed lines into the database.
+	 */
+	private DatabaseManager dbManager;
+
+	private ArrayList<Table> tables = new ArrayList<Table>();
+	
+	public GffParser(DatabaseManager dbManager) {
+		this.dbManager = dbManager;
+	}
+	
+	/**
+	 * Reads through the gff file, parsing all the infomation into the database
+	 * 
+	 * @param file
+	 * 				Path to the gff file.
+	 * @throws GffException
+	 * 				Exception thrown when execution fails.
+	 */
+	public void parse(String file) throws GffException {
+		tables.add(new AnnotationTable());
+		dbManager.createTables(tables);
+		
+		String line;
+		try {
+			BufferedReader bufferedReader = 
+					new BufferedReader(new InputStreamReader(new FileInputStream(file), 
+							Charset.defaultCharset()));
+		    while ((line = bufferedReader.readLine()) != null) {
+		    	parseLine(line);
+		    }
+		    
+		    bufferedReader.close();
+		    
+		} catch (FileNotFoundException e) {
+			System.err.println("File not found: " + file);
+			e.printStackTrace();
+		} catch (IOException e) {
+			System.err.println("Error reading file: " + file);
+			e.printStackTrace();
+		}
+	}
+	
+	/**
+	 * Method that parses the line into a row that can be inserted into the table
+	 * 
+	 * @param line
+	 * 			Link line to parse.
+	 */
+	private void parseLine(String line) {
+		
+	}	
 }
