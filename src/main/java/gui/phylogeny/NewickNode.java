b84d6b5dccf9ef69b32ae8ed53cac0a1e5494f56
Evan <pickleo.joe@gmail.com>
2016-06-02 17:15:11 +0200

    Scaling branch length and interactable tree

--- src/main/java/gui/phylogeny/NewickNode.java
+++ src/main/java/gui/phylogeny/NewickNode.java
@@ -1,8 +1,13 @@
 package gui.phylogeny;
 
+import javafx.event.EventHandler;
 import javafx.scene.Group;
 import javafx.scene.control.Label;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.paint.Paint;
 import javafx.scene.shape.Rectangle;
+import javafx.scene.shape.Shape;
 
 /**
  * Class that is used to visualize a single node of a {@link NewickTree}
@@ -11,8 +16,69 @@ public class NewickNode extends Group {
 	
 	private static final int SIZE = 10;
 	private Rectangle node = new Rectangle(0 - SIZE / 2, 0 - SIZE / 2, SIZE, SIZE);
+	private Label label = null;
+	public boolean isLeaf = false;
+	
+	/**
+	 * Event handler for mouse click event with the phylogenetic view.
+	 */
+	private final EventHandler<MouseEvent> mouseEventHandler = new EventHandler<MouseEvent>() {
+		
+		@Override
+		public void handle(MouseEvent event) {
+			Paint currentColor = node.getFill();
+			if (currentColor.toString().equals("0x000000ff")) {
+				node.setFill(Paint.valueOf("#778899"));
+				if (label != null) {
+					label.setTextFill(Paint.valueOf("#778899"));
+				}
+				turnChildrenGrey();
+				
+			} else {
+				node.setFill(Paint.valueOf("0x000000ff"));
+				if (label != null) {
+					label.setTextFill(Paint.valueOf("0x000000ff"));
+				}
+				turnChildrenBlack();
+			}
+		}
+	};
+	
+	/**
+	 * Colors all relevant children in the Group black.
+	 */
+	private void turnChildrenBlack() {
+		for (Object child : this.getChildren()) {
+			if (child instanceof NewickNode) {
+				((NewickNode) child).getRectangle().setFill(Paint.valueOf("0x000000ff"));
+				if (label != null) {
+					((NewickNode) child).getLabel().setTextFill(Paint.valueOf("0x000000ff"));
+				}
+				if (!child.)
+			} else if (child instanceof NewickEdge) {
+				((NewickEdge) child).setFill(Paint.valueOf("0x000000ff"));
+			}
+		}
+	}
+	
+	/**
+	 * Colors all relevant children in the Group light grey.
+	 */
+	private void turnChildrenGrey() {
+		for (Object child : this.getChildren()) {
+			if (child instanceof NewickNode) {
+				((NewickNode) child).getRectangle().setFill(Paint.valueOf("#778899"));
+				if (label != null) {
+					((NewickNode) child).getLabel().setTextFill(Paint.valueOf("#778899"));
+				}
+			} else if (child instanceof NewickEdge) {
+				((NewickEdge) child).setFill(Paint.valueOf("#778899"));
+			}
+		}
+	}
 	
 	public NewickNode() {
+		node.addEventFilter(MouseEvent.MOUSE_CLICKED, mouseEventHandler);
 		this.getChildren().add(node);
 	}
 	
@@ -28,14 +94,23 @@ public class NewickNode extends Group {
 		this.addLabel(name);
 	}
 	
+	public Rectangle getRectangle() {
+		return node;
+	}
+	
+	public Label getLabel() {
+		return label;
+	}
+	
 	public void hideRectangle() {
 		node.setVisible(false);
 	}
 	
 	private void addLabel(String text) {
-		Label label = new Label(text);
+		label = new Label(text);
 		label.setTranslateX(10);
 		label.setTranslateY(-8);
+		label.addEventFilter(MouseEvent.MOUSE_CLICKED, mouseEventHandler);
 		this.getChildren().add(label);
 	}
 	
