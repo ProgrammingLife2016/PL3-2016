c86a89f050207cd29497762b1f0ba69aa5c332d7
Evan <pickleo.joe@gmail.com>
2016-06-03 02:51:48 +0200

    Animated expanding and contracting

--- src/main/java/gui/PhyloMenuController.java
+++ src/main/java/gui/PhyloMenuController.java
@@ -3,12 +3,15 @@ package gui;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.ResourceBundle;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.Group;
 import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.input.ScrollEvent;
 import javafx.scene.control.Button;
 import javafx.scene.control.ScrollPane;
@@ -16,6 +19,7 @@ import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.GridPane;
 import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
 import javafx.scene.shape.Line;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Text;
@@ -34,13 +38,66 @@ public class PhyloMenuController implements Initializable {
 	@FXML private AnchorPane anchorPane;
 	@FXML private Button expandButton;
 	
+	private boolean expanded = false;
 	private GridPane outerPane;
 
+	/**
+	 * Event handler for mouse click event with the phylogenetic view.
+	 */
+	private final EventHandler<MouseEvent> expandButtonHandler = new EventHandler<MouseEvent>() {
+		
+		@Override
+		public void handle(MouseEvent event) {
+			if (expanded == false) {
+				Timer animTimer = new Timer();
+			    animTimer.scheduleAtFixedRate(new TimerTask() {
+
+			        int i=0;
+
+			        @Override
+			        public void run() {
+			            if (i<35){
+
+			            	outerPane.getRowConstraints().get(1).setPrefHeight(outerPane.getRowConstraints().get(1).getPrefHeight() + 20);
+			            }
+			            else {
+			                this.cancel();
+			            }
+
+			            i++;
+			        }
+			    }, 2000, 25);
+				expanded = true;
+			} else if (expanded == true) {
+				Timer animTimer = new Timer();
+			    animTimer.scheduleAtFixedRate(new TimerTask() {
+
+			        int i=0;
+
+			        @Override
+			        public void run() {
+			            if (i<25){
+
+			            	outerPane.getRowConstraints().get(1).setPrefHeight(outerPane.getRowConstraints().get(1).getPrefHeight() - 20);
+			            }
+			            else {
+			                this.cancel();
+			            }
+
+			            i++;
+			        }
+			    }, 2000, 25);
+				expanded = false;
+			}
+		}
+	};
+	
 	/**
 	 * Initialize fxml file.
 	 */
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
+		expandButton.addEventHandler(MouseEvent.MOUSE_CLICKED, expandButtonHandler);
 		pane.boundsInParentProperty().addListener((observable, oldValue, newValue) -> {
 		    scrollPane.setPrefWidth(newValue.getWidth());
 		    scrollPane.setPrefHeight(newValue.getHeight());
